f1d92cabd1dd023826511edb379168d6bbcc4e9d
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-09-15 13:45:44 +0530

    adding the model to the login controller

--- src/main/java/com/example/springboottodo/LoginController.java
+++ src/main/java/com/example/springboottodo/LoginController.java
@@ -2,7 +2,9 @@ package com.example.springboottodo;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -10,7 +12,9 @@ public class LoginController {
 
 
     @RequestMapping("/login")
-    public String login(){
+    public String login(@RequestParam String name, ModelMap model){
+        model.put("name",name);
+        
         return "login";
     }
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -4,5 +4,6 @@
 </head>
 <body>
 <h1>Hey this is the login page - JSP</h1>
+<h1>Welcome ${name}</h1>
 </body>
 </html>
