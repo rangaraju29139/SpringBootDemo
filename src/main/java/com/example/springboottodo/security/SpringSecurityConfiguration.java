a49d41334806b492be4ffff70e285775793811cc
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-09-21 17:24:24 +0530

    added the http security

--- src/main/java/com/example/springboottodo/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/springboottodo/security/SpringSecurityConfiguration.java
@@ -2,11 +2,15 @@ package com.example.springboottodo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -36,5 +40,26 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws  Exception{
+//        httpSecurity.authorizeHttpRequests(auth->auth.anyRequest().authenticated());
+//        httpSecurity.formLogin(Customizer.withDefaults());
+//        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+//
+//        httpSecurity.headers(Customizer.withDefaults())
+
+        httpSecurity
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+
+
+        return httpSecurity.build();
     }
 }
